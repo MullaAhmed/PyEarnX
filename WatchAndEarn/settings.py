c5172e7f1a8671dccfcf5d74dc55f5ae9464b516
Ahmed Mulla <67974888+MullaAhmed@users.noreply.github.com>
2022-07-17 19:12:06 +0530

    Update settings.py

--- WatchAndEarn/settings.py
+++ WatchAndEarn/settings.py
@@ -139,9 +139,9 @@ import os
 AUTH_USER_MODEL="Authentication.User"
 
 CORS_ALLOW_ALL_ORIGINS=True
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:3000',
-)
+CORS_ORIGIN_WHITELIST = [
+  'http://localhost:3000'
+]
 
 REST_FRAMEWORK={
         'DEFAULT_AUTHENTICATION_CLASSES':[
